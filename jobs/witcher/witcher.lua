28f7845f5f6ee9055ee5789646e19a002e2d0a85
Thahat <39244853+Thahat@users.noreply.github.com>
2020-10-12 22:08:25 +0200

    update

--- jobs/witcher/witcher.lua
+++ jobs/witcher/witcher.lua
@@ -1,4 +1,8 @@
 local WitcherClass = class()
 local CombatJob = require 'stonehearth.jobs.combat_job'
+local CraftingJob = require 'stonehearth.jobs.crafting_job'
+
 radiant.mixin(WitcherClass, CombatJob)
+radiant.mixin(WitcherClass, CraftingJob)
+
 return WitcherClass
